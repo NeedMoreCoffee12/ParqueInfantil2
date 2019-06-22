58083bfda05cc3e590c6fe48de04b22f27452e8f
ucran <ucran@169.254.99.253>
2019-06-22 19:20:56 +0200

    test3

--- src/interfaces/InterfazParque.java
+++ src/interfaces/InterfazParque.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *SUNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNORMAL
  */
 package interfaces;
 
@@ -17,7 +15,7 @@ import java.util.logging.Logger;
 
 /**
  *
- * @author jonat
+ * @author ucran
  */
 public class InterfazParque extends javax.swing.JFrame {
 
